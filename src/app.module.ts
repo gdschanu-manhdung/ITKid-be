249d223e40a93ed71966cf9b81776248b854432d
Maduro29 <manhdungtran.vnuuet@gmail.com>
2024-03-09 00:04:09 +0700

    config: connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config'
+import { DatabaseModule } from './database/database.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+        DatabaseModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            autoLoadEntities: true,
+            entities: ['dist/database/typeorm/entities/*.js'],
+            synchronize: true,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        })
+    ],
+    controllers: [],
+    providers: []
 })
 export class AppModule {}
