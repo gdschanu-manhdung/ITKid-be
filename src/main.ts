fdf1adbae45d53987ba16fa659f63364ed9f285d
Maduro29 <manhdungtran.vnuuet@gmail.com>
2024-03-14 03:50:41 +0700

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as bodyParser from 'body-parser'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
@@ -8,6 +9,14 @@ async function bootstrap() {
     app.setGlobalPrefix('api')
     app.enableCors()
     app.use(bodyParser.json())
+    const config = new DocumentBuilder()
+        .setTitle('Cats example')
+        .setDescription('The cats API description')
+        .setVersion('1.0')
+        .addTag('cats')
+        .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('api', app, document)
     try {
         await app.listen(PORT, () =>
             console.log(`Server listening on PORT ${PORT}`)
