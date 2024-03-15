33da208bb3bb5a5ad73ad9318a73d1ea2e999680
Maduro29 <manhdungtran.vnuuet@gmail.com>
2024-03-15 15:45:03 +0700

    feat: create addCategory api

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, HttpStatus, Inject, Res } from '@nestjs/common'
+import {
+    Controller,
+    Get,
+    HttpStatus,
+    Inject,
+    Post,
+    Req,
+    Res
+} from '@nestjs/common'
 import { Routes, Services } from 'src/utils/constants'
 import { CategoriesService } from './categories.service'
-import { Response } from 'express'
+import { Response, Request } from 'express'
+import { ApiBody } from '@nestjs/swagger'
+import { AddCategoryDto } from './dto/AddCategory.dto'
 
 @Controller(Routes.CATEGORIES)
 export class CategoriesController {
@@ -14,6 +24,16 @@ export class CategoriesController {
     async getAllCategories(@Res() res: Response) {
         return res.status(HttpStatus.OK).json({
             categories: await this.categoriesService.getAllCategories()
+        })
+    }
+
+    @ApiBody({ type: AddCategoryDto })
+    @Post('addCategory')
+    async addCategory(@Req() req: Request, @Res() res: Response) {
+        const addCategoryDto = req.body as AddCategoryDto
+
+        return res.status(HttpStatus.OK).json({
+            category: await this.categoriesService.addCategory(addCategoryDto)
         })
     }
 }
--- src/categories/categories.ts
+++ src/categories/categories.ts
@@ -1,5 +1,7 @@
 import { Category } from 'src/database/typeorm/entities/Category'
+import { AddCategoryDto } from './dto/AddCategory.dto'
 
 export interface ICategoriesService {
     getAllCategories(): Promise<Category[]>
+    addCategory(addCategoryDto: AddCategoryDto): Promise<Category>
 }
