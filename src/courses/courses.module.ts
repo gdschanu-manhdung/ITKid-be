e708bff8c63a3409756bbee0c74f55197fcefc7f
Maduro29 <manhdungtran.vnuuet@gmail.com>
2024-03-17 17:52:18 +0700

    fix: fix deleteCourse api

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Category } from 'src/database/typeorm/entities/Category'
 import { Course } from 'src/database/typeorm/entities/Course'
+import { Lesson } from 'src/database/typeorm/entities/Lesson'
+import { Test } from 'src/database/typeorm/entities/Test'
 import { User } from 'src/database/typeorm/entities/User'
 import { Services } from 'src/utils/constants'
 import { CoursesController } from './courses.controller'
 import { CoursesService } from './courses.service'
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Course, Category, User])],
+    imports: [TypeOrmModule.forFeature([Course, Category, User, Test, Lesson])],
     controllers: [CoursesController],
     providers: [
         {
